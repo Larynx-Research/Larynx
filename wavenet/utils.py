9a2216b832d9fb08fcbb9a04f12d09d9256268e2
Shauray <shauray9@gmail.com>
2023-03-08 15:12:07 +0530

    load data set

--- wavenet/utils.py
+++ wavenet/utils.py
@@ -8,6 +8,7 @@ import numpy as np
 import numbers
 import types
 import argparse
+import pandas as pd
 
 import torch
 import torchaudio
@@ -20,17 +21,25 @@ def data_seeker(root):
     print(np.array(input_img).shape)
     return input_img
 
+def annote(csv_root, train_data):
+    local_buffer = {}
+    df = pd.read_csv(csv_root,index)
+    for i in train_data:
+        local_buffer[i.split("\\")[1]] = df[(i.split("\\")[1]).split(".")[0]]
 
-def load_dataset(root, transform=None, split=None):
+    print(local_buffer)
+    return local_buffer
+
+def load_dataset(root, csv_root, transform=None, split=None):
     train_data = data_seeker(root)
-    global_buffer = {}
-    for i in train_data[:1]:
-        waveform, sample_rate = torchaudio.load(i)
-        global_buffer[i.split("\\")[1]] = waveform
+    annotations = annote(csv_root,train_data)
+
+
+def lazy_load_dataset(wav_file):
+    waveform, sample_rate = torchaudio.load(wav_file)
+    global_buffer[i.split("\\")[1]] = waveform
 
     print(global_buffer)
-
-def lazy_load_dataset(root, transform, split):
     pass
 
 
@@ -39,8 +48,9 @@ def lazy_load_dataset(root, transform, split):
 
 
 root = "E:/data/LJSpeech-1.1/wavs/*"
+csv_root = "E:/data/LJSpeech-1.1/metadata.csv"
 
 
-load_dataset(root)
+load_dataset(root, csv_root)
 
 
