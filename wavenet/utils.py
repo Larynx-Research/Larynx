3a8654c7e76ae12530bff7be4e72550366344d5a
Shauray <shauray9@gmail.com>
2023-03-09 23:19:45 +0530

    data load

--- wavenet/utils.py
+++ wavenet/utils.py
@@ -15,24 +15,34 @@ import torchaudio
 from torch.utils.data import DataLoader, Dataset
 import pickle
 
+CHARSET = " abcdefghijklmnopqrstuvwxyz,.'"
 
 def data_seeker(root):
-    input_img = (glob.glob(os.path.join(root)))
+    input_img = (glob.glob(os.path.join(root,"wavs/*")))
     print(np.array(input_img).shape)
     return input_img
 
-def annote(csv_root, train_data):
-    local_buffer = {}
-    df = pd.read_csv(csv_root,index)
-    for i in train_data:
-        local_buffer[i.split("\\")[1]] = df[(i.split("\\")[1]).split(".")[0]]
+def annote(root, train_data):
+    local_buffer = []
 
-    print(local_buffer)
+    with open(os.path.join(root, "metadata_cp.txt"), encoding="utf8") as txtfile:
+        corpus = txtfile.read().split("\n")
+        for row in corpus:
+            row = row.split("|")
+            tokens = [CHARSET.index(c)+1 for c in row[1].lower() if c in CHARSET]
+            if len(tokens) <= 250:
+                local_buffer.append((os.path.join(root, 'wavs', row[0]+".wav"), tokens))
+    print("got metadata", len(local_buffer))
     return local_buffer
 
-def load_dataset(root, csv_root, transform=None, split=None):
+def load_dataset(root, transform=None, split=0):
     train_data = data_seeker(root)
-    annotations = annote(csv_root,train_data)
+    annotations = annote(root,train_data)
+
+    if split:
+        train,validate = annotations[0:split], annotations[split:]
+        return train, validate
+    return annotations, []
 
 
 def lazy_load_dataset(wav_file):
@@ -40,17 +50,10 @@ def lazy_load_dataset(wav_file):
     global_buffer[i.split("\\")[1]] = waveform
 
     print(global_buffer)
-    pass
+
+root = "E:/data/LJSpeech-1.1/"
 
 
-
-
-
-
-root = "E:/data/LJSpeech-1.1/wavs/*"
-csv_root = "E:/data/LJSpeech-1.1/metadata.csv"
-
-
-load_dataset(root, csv_root)
+load_dataset(root)
 
 
